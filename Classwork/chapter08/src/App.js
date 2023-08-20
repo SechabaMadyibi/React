2f1e1a759de7e5b82e078203f79f0c64248404bc
SechabaMadyibi <sechabamadyibi1@gmail.com>
2023-08-21 01:35:47 +0200

    21 August

--- Classwork/chapter08/src/App.js
+++ Classwork/chapter08/src/App.js
@@ -1,18 +1,1 @@
-import './App.css';
-import React, { Component } from 'react';
-//import JumboTronComponent from './JumboTronComponent';
-import UserForm from './UserForm';
-//import Products from './Products';
-import GitHub from './GitHub';
-
-class App extends Component {
- render() {
- return (
- <div>
- <GitHub />
- </div>
- );
- }
-}
-export default App;
 
